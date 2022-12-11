a84215c34ae92fc8f00eb2c829a29ef22de8e311
Sergey Morkovkin <sergeymorkovkin@gmail.com>
2022-12-11 13:30:20 +0100

    Initial commit

---
+++ src/test-bokeh-example/test-bokeh-example.story.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Dimensions, View } from 'react-native'
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import { styles } from './test-bokeh-example.styles'
+
+const { width, height } = Dimensions.get('window')
+
+function getRandomWidth() {
+  return Math.random() * width
+}
+
+function getRandomHeight() {
+  return Math.random() * height
+}
+
+function getRandomHue() {
+  return 100 + Math.random() * 100
+}
+
+function getRandomPositionDiff() {
+  return -100 + Math.random() * 200
+}
+
+function getRandomHueDiff() {
+  return Math.random() * 100
+}
+
+function Circle() {
+  const left = useSharedValue(getRandomWidth())
+  const top = useSharedValue(getRandomHeight())
+  const hue = useSharedValue(getRandomHue())
+
+  const duration = 2000 + Math.random() * 1000
+  const power = Math.random()
+  const config = { duration, easing: Easing.linear }
+
+  const update = () => {
+    left.value = withTiming(left.value + getRandomPositionDiff(), config)
+    top.value = withTiming(top.value + getRandomPositionDiff(), config)
+    hue.value = withTiming(hue.value + getRandomHueDiff(), config)
+  }
+
+  React.useEffect(() => {
+    update()
+    const id = setInterval(update, duration)
+    return () => clearInterval(id)
+  })
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const size = 100 + power * 250
+    return {
+      backgroundColor: `hsl(${hue.value}, 100%, 50%)`,
+      width: size,
+      height: size,
+      top: top.value - size / 2,
+      left: left.value - size / 2,
+      opacity: 0.1 + (1 - power) * 0.1,
+      // transform: [
+      //   { translateX: top.value - size / 2 },
+      //   { translateY: left.value - size / 2 },
+      // ]
+    }
+  }, [])
+
+  return <Animated.View style={[styles.Bokeh, animatedStyle]} />
+}
+
+interface BokehProps {
+  count: number
+}
+
+function Bokeh({ count }: BokehProps) {
+  return (
+    <>
+      {[...Array(count)].map((_, i) => (
+          <Circle key={i} />
+      ))}
+    </>
+  )
+}
+
+export function TestBokehExampleStory() {
+  return (
+    <View style={styles.Container}>
+      <Bokeh count={100} />
+    </View>
+  )
+}
---
+++ src/test-bokeh-example/test-bokeh-example.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+
+  Container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    overflow: 'hidden',
+  },
+
+  Bokeh: {
+    position: 'absolute',
+    borderRadius: 9999,
+  },
+
+})
